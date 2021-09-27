9a4959073fbc909933f01af72fe90f264fc6c912
aditikisara <adita4298@gmail.com>
2021-09-27 11:29:23 -0700

    SQL code + Output file
    Find the total account balance of all the customers from EUROPE in the MACHINERY market segment.

---
+++ lab-3/test/13.sql
@@ -0,0 +1,11 @@
+SELECT
+    SUM(c_acctbal)
+FROM
+    customer,
+    region,
+    nation
+WHERE
+    c_nationkey = n_nationkey AND
+    n_regionkey = r_regionkey AND
+    r_name = 'EUROPE' AND
+    c_mktsegment = 'MACHINERY';
